fe7674b3cbfe6347c1b5c4a0456ebd59b92fb1b9
tian-lt <tilia@microsoft.com>
2022-11-29 22:03:19 +0800

    poc1

--- MultithreadedWindow/MultithreadedWindow.cpp
+++ MultithreadedWindow/MultithreadedWindow.cpp
@@ -3,10 +3,14 @@
 
 #include "MultithreadedWindow.h"
 
+#include <cassert>
 #include <format>
+#include <latch>
+#include <memory>
 #include <mutex>
 #include <string>
 #include <thread>
+#include <vector>
 
 #include "framework.h"
 
@@ -18,114 +22,13 @@ WCHAR szTitle[MAX_LOADSTRING];        // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];  // the main window class name
 HWND childhwnd;
 HWND tophwnds[2];
+std::latch all_done{3};
 
 // Forward declarations of functions included in this code module:
 void MyRegisterClass(HINSTANCE hInstance);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK ChildProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
 
-constexpr unsigned TM_BASE = WM_USER + 1000;
-constexpr unsigned TM_1 = TM_BASE + 1;
-constexpr unsigned TM_2 = TM_BASE + 2;
-
-void dump_msg(HWND hwnd, UINT msg, const std::string& tag) {
-  if (msg < TM_BASE) return;
-  auto parent = GetParent(childhwnd);
-  auto tid = GetCurrentThreadId();
-  OutputDebugStringA(
-      std::format("tag = {}, tid = {}, hparent = {} hwnd = {}, msg = {}\n", tag,
-                  tid, reinterpret_cast<unsigned long long>(parent),
-                  reinterpret_cast<unsigned long long>(hwnd), msg)
-          .c_str());
-}
-
-void test() {
-  static int state = 0;
-  switch (state) {
-    case 0: {
-      // post a test message
-      PostMessage(childhwnd, TM_1, 0, 0);
-      state = 1;
-    } break;
-    case 1: {
-      // move child to window 1.
-      auto from = GetWindowThreadProcessId(tophwnds[0], nullptr);
-      auto to = GetWindowThreadProcessId(tophwnds[1], nullptr);
-      if (!AttachThreadInput(from, to, TRUE)) {
-        auto err = GetLastError();
-        throw err;
-      }
-      SetParent(childhwnd, tophwnds[1]);
-      state = 2;
-    } break;
-    case 2: {
-      // post a test message
-      PostMessage(childhwnd, TM_1, 0, 0);
-    } break;
-  }
-}
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                      _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine,
-                      _In_ int nCmdShow) {
-  UNREFERENCED_PARAMETER(hPrevInstance);
-  UNREFERENCED_PARAMETER(lpCmdLine);
-  hInst = hInstance;
-
-  // TODO: Place code here.
-
-  // Initialize global strings
-  LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-  LoadStringW(hInstance, IDC_MULTITHREADEDWINDOW, szWindowClass,
-              MAX_LOADSTRING);
-  MyRegisterClass(hInstance);
-
-  auto proc = [&](int wid) {
-    tophwnds[wid] = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-                                  CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr,
-                                  nullptr, hInstance, nullptr);
-    if (!tophwnds[wid]) {
-      throw "failed to create top window 0.";
-    }
-
-    if (wid == 0) {
-      childhwnd =
-          CreateWindowEx(0, TEXT("ChildWndClass"), L"", WS_CHILD | WS_BORDER, 0,
-                         0, 100, 100, tophwnds[wid], nullptr, hInst, nullptr);
-      if (!childhwnd) {
-        throw "failed to create the childwindow.";
-      }
-
-      ShowWindow(childhwnd, SW_SHOWDEFAULT);
-      UpdateWindow(childhwnd);
-    }
-
-    ShowWindow(tophwnds[wid], nCmdShow);
-    UpdateWindow(tophwnds[wid]);
-
-    HACCEL hAccelTable =
-        LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MULTITHREADEDWINDOW));
-
-    MSG msg;
-    while (GetMessage(&msg, nullptr, 0, 0)) {
-      if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg)) {
-        dump_msg(msg.hwnd, msg.message, std::format("Topwnd[{}]", wid));
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-      }
-    }
-  };
-
-  std::jthread t1{proc, 0};
-  std::jthread t2{proc, 1};
-}
-
-//
-//  FUNCTION: MyRegisterClass()
-//
-//  PURPOSE: Registers the window class.
-//
 void MyRegisterClass(HINSTANCE hInstance) {
   {
     WNDCLASSEXW wcex;
@@ -157,18 +60,121 @@ void MyRegisterClass(HINSTANCE hInstance) {
   }
 }
 
-//
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  PURPOSE: Processes messages for the main window.
-//
-//  WM_COMMAND  - process the application menu
-//  WM_PAINT    - Paint the main window
-//  WM_DESTROY  - post a quit message and return
-//
-//
+constexpr unsigned TM_BASE = WM_USER + 1000;
+constexpr unsigned TM_1 = TM_BASE + 1;
+constexpr unsigned TM_2 = TM_BASE + 2;
+constexpr unsigned TM_WID = TM_BASE + 1000;
+constexpr unsigned TM_CLOSE = TM_BASE + 1001;
+
+void dump_msg(HWND hwnd, UINT msg, const std::string& tag) {
+  if (msg < TM_BASE) return;
+  auto parent = GetParent(childhwnd);
+  auto tid = GetCurrentThreadId();
+  OutputDebugStringA(
+      std::format("tag = {}, tid = {}, hparent = {} hwnd = {}, msg = {}\n", tag,
+                  tid, reinterpret_cast<unsigned long long>(parent),
+                  reinterpret_cast<unsigned long long>(hwnd), msg)
+          .c_str());
+}
+
+void test() {
+  static int state = 0;
+  switch (state) {
+    case 0: {
+      // post a test message
+      PostMessage(childhwnd, TM_1, 0, 0);
+      state = 1;
+    } break;
+    case 1: {
+      // move child to window 1.
+      SetParent(childhwnd, tophwnds[1]);
+      PostMessage(childhwnd, TM_WID, 0, 1);
+      state = 2;
+    } break;
+    case 2: {
+      // post a test message
+      PostMessage(childhwnd, TM_1, 0, 0);
+    } break;
+  }
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                      _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine,
+                      _In_ int nCmdShow) {
+  UNREFERENCED_PARAMETER(hPrevInstance);
+  UNREFERENCED_PARAMETER(lpCmdLine);
+  hInst = hInstance;
+
+  // TODO: Place code here.
+
+  // Initialize global strings
+  LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+  LoadStringW(hInstance, IDC_MULTITHREADEDWINDOW, szWindowClass,
+              MAX_LOADSTRING);
+  MyRegisterClass(hInstance);
+
+  auto topwnd_proc = [&](int wid) {
+    tophwnds[wid] = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+                                  CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr,
+                                  nullptr, hInstance, nullptr);
+    if (!tophwnds[wid]) {
+      throw "failed to create top window 0.";
+    }
+    ShowWindow(tophwnds[wid], nCmdShow);
+    UpdateWindow(tophwnds[wid]);
+    PostMessage(tophwnds[wid], TM_WID, 0, wid);
+
+    if (wid == 0) {
+      auto childwnd_proc = [hInst = hInst](int wid) {
+        assert(wid == 0);
+        childhwnd = CreateWindowEx(0, TEXT("ChildWndClass"), L"",
+                                   WS_CHILD | WS_BORDER, 0, 0, 100, 100,
+                                   tophwnds[wid], nullptr, hInst, nullptr);
+        if (!childhwnd) {
+          throw "failed to create the childwindow.";
+        }
+
+        ShowWindow(childhwnd, SW_SHOWDEFAULT);
+        UpdateWindow(childhwnd);
+        PostMessage(childhwnd, TM_WID, 0, wid);
+
+        HACCEL hAccelTable =
+            LoadAccelerators(hInst, MAKEINTRESOURCE(IDC_MULTITHREADEDWINDOW));
+        MSG msg;
+        while (GetMessage(&msg, nullptr, 0, 0)) {
+          if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg)) {
+            dump_msg(msg.hwnd, msg.message,
+                     std::format("childwnd_proc[{}]", wid));
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+          }
+        }
+        all_done.count_down();
+      };
+      std::thread{childwnd_proc, 0}.detach();
+    }
+
+    HACCEL hAccelTable =
+        LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MULTITHREADEDWINDOW));
+    MSG msg;
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+      if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg)) {
+        dump_msg(msg.hwnd, msg.message, std::format("topwnd_proc[{}]", wid));
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+      }
+    }
+    all_done.count_down();
+  };
+
+  std::thread{topwnd_proc, 0}.detach();
+  std::thread{topwnd_proc, 1}.detach();
+  all_done.wait();
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam,
                          LPARAM lParam) {
+  static thread_local int wid = -1;
   switch (message) {
     case WM_COMMAND: {
       int wmId = LOWORD(wParam);
@@ -190,32 +196,41 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam,
       // TODO: Add any drawing code that uses hdc here...
       EndPaint(hWnd, &ps);
     } break;
-    case WM_DESTROY:
-      PostQuitMessage(0);
+    case TM_WID:
+      wid = static_cast<int>(lParam);
       break;
+    case WM_DESTROY: {
+      PostMessage(childhwnd, TM_CLOSE, 0, wid);
+      PostQuitMessage(0);
+    } break;
     default:
       return DefWindowProc(hWnd, message, wParam, lParam);
   }
   return 0;
 }
 
-// Message handler for about box.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
-  UNREFERENCED_PARAMETER(lParam);
-  switch (message) {
-    case WM_INITDIALOG:
-      return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-      if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
-        return (INT_PTR)TRUE;
-      }
-      break;
-  }
-  return (INT_PTR)FALSE;
-}
-
 LRESULT CALLBACK ChildProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
-  dump_msg(hwnd, msg, "ChildProc");
-  return DefWindowProc(hwnd, msg, wparam, lparam);
+  dump_msg(hwnd, msg, "ChildProcCallback");
+  static thread_local int wid = -1;
+  switch (msg) {
+    case TM_WID:
+      wid = static_cast<int>(lparam);
+      break;
+    case WM_LBUTTONUP: {
+      auto tid = GetCurrentThreadId();
+      OutputDebugStringA(
+          std::format("ChildProcCallback: WM_LBUTTONUP, tid = {}\n", tid)
+              .c_str());
+    } break;
+    case TM_CLOSE: {
+      auto wid_from = static_cast<int>(lparam);
+      if (wid_from == wid) {
+        OutputDebugStringA("The childwnd_proc is going to die.\n");
+        PostQuitMessage(0);
+      }
+    } break;
+    default:
+      return DefWindowProc(hwnd, msg, wparam, lparam);
+  }
+  return 0;
 }
